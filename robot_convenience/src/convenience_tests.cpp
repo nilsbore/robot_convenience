7b906e485f4cae56e3c7b228341d2ec14966e44f
Nils Bore <nilsbore@gmail.com>
2016-09-26 12:38:07 +0200

    Fixed bug

--- robot_convenience/src/convenience_tests.cpp
+++ robot_convenience/src/convenience_tests.cpp
@@ -4,6 +4,7 @@
 #include <cereal/types/vector.hpp>
 #include <eigen_cereal/eigen_cereal.h>
 #include <ros_cereal/ros_cereal.h>
+#include <mongodb_store_associated/message_store_associated.h>
 #include <iostream>
 
 using namespace std;
