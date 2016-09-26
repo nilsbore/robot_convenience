221fd5dced4d88bb8dd948fea22ab9040a393365
Nils Bore <nilsbore@gmail.com>
2016-09-26 11:35:18 +0200

    Added the robot convenience meta package

---
+++ robot_convenience/src/convenience_tests.cpp
@@ -0,0 +1,16 @@
+#include <simple_graph/simple_graph.h>
+#include <simple_graph/graphviz.h>
+#include <cereal/archives/binary.hpp>
+#include <cereal/types/vector.hpp>
+#include <eigen_cereal/eigen_cereal.h>
+#include <ros_cereal/ros_cereal.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    cout << "It's running!" << endl;
+
+    return 0;
+}
